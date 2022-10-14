d8374d4fef6625cb98c89b08d7321a490966bfee
Anubz <elias-fausto@hotmail.com>
2022-10-14 15:59:33 -0300

    Configurando testes e Mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Envelope, TextIndent, Lock } from 'phosphor-react';
+import { Button } from './components/Button';
+import { Checkbox } from './components/Checkbox';
+import { Heading } from './components/Heading';
+import { SignIn } from './components/pages/SignIn';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Logo } from './Logo';
 import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-    </>
+    <SignIn/>
   )
 }
